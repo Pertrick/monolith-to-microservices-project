99b752b903377151490d77e862809e7d242bc761
Patrick Udoh <udohpertrick@gmail.com>
2022-08-06 17:11:47 -0400

    changes made to frontend

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a9f04971b16644d2fa78e88d53e8cf2e-1773293899.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
