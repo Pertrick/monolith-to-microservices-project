d38c97196c1883a15181cc040e2299c93fe0a909
Patrick Udoh <udohpertrick@gmail.com>
2022-08-06 12:59:15 -0400

    monolith to Microservices project

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=`#Twenty2020`
+export POSTGRES_HOST=database-1.cgjecx9p7k8q.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=arn:aws:s3:::patrickudoh-bucket
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
